aa6cd4c791dc59667664141315e35ead2efac13b
Eduardo Sacramento <eduardo.sacramento@neomove.com.br>
2018-02-01 02:36:30 -0200

    Add arquivo index da aplicacao

---
+++ src/Controller/Template.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: edusacra
+ * Date: 01/02/18
+ * Time: 01:57
+ */
+
+namespace Controller;
+
+
+class Template
+{
+
+}
---
+++ src/View/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: edusacra
+ * Date: 01/02/18
+ * Time: 01:56
+ */
